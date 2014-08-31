9d0c5bef93c79673befdc82c3f9cd0bc993da64e
Rolf Schmutz <rschmutz@netlabs.ch>
2014-09-01 00:22:53 +0200

    persistent states (buggy)

--- sm.rb
+++ sm.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/ruby
 # 20140830 rschmutz@netlabs.ch
 
+require "pstore"
+
+
+# control collection of services
+
 class Controller
   @services = []
+  @states = nil
 
   def initialize(directory)
+    @states = PStore.new(directory + "/" + ".states")
     @services = Array.new
     Dir.foreach directory do |file|
       next if file.match /^\./
@@ -22,84 +29,110 @@ class Controller
 
   def add_service(service)
     @services << service
+    @states.transaction do
+      begin
+        service.state = @states[service.name]
+      rescue
+        @states[service.name] = service.state
+        @states.commit
+      end
+    end
+  end
+
+  def start_service(service)
+    service.start_commands.each do |command|
+      service.processes << fork do
+        exec command
+      end
+    end
+    service.processes.each { |pid| Process::detach pid }
+    @states.transaction do
+      @states[service.name] = (service.state = :running)
+      @states.commit
+    end
   end
 
   def start
     @services.each do |service|
-      service.start_commands.each do |command|
-        service.processes << fork do
-          exec command
+      start_service service
+    end
+  end
+
+  def stop_service(service)
+    if service.stop_commands.empty? # kill processes the old way
+      pid_from_files = service.pidfiles.collect do |pidfile|
+        begin
+          File.read(pidfile)
+        rescue
         end
       end
-      service.processes.each { |pid| Process::detach pid }
-      service.state = :running
+      (service.processes & pid_from_files).each do |pid|
+        begin
+          Process::kill "KILL", pid
+        rescue
+        end
+      end
+    else # use provided stop commands
+      service.stop_commands.each do |command|
+        system command
+      end
+    end
+    @states.transaction do
+      @states[service.name] = service.state = :stopped
+      @states.commit
     end
   end
 
   def stop
     @services.each do |service|
-      if service.stop_commands.empty? # kill processes the old way
-        pid_from_files = service.pidfiles.collect do |pidfile|
+      stop_service service
+    end
+  end
+
+  def status_service(service)
+    check = nil
+    if service.status_commands.empty? # kill processes the old way
+      if service.pidfiles.empty?
+        pids = service.processes
+      else
+        pids = service.pidfiles.collect do |pidfile|
           begin
             File.read(pidfile)
           rescue
           end
         end
-        (service.processes & pid_from_files).each do |pid|
-          begin
-            Process::kill "KILL", pid
-          rescue
-          end
-        end
-      else # use provided stop commands
-        service.stop_commands.each do |command|
-          system command
+      end
+      if pids.all? do |pid|
+        begin
+          Process::kill 0, pid
+          check = :running 
+        rescue
+          check = :stopped
         end
       end
-      service.state = :stopped
+      end
+    else # use provided status commands
+      service.status_commands.all? do |command|
+        fork { exec command }
+      end
     end
+    result = :unknown
+    if service.state != :uninitialized
+      if check == service.state
+        result = :running
+      else
+        result = :stopped
+      end
+    end
+    return result
   end
 
   def status
     printf "%-20s%-15s%s\n", "service", "state", "check"
     printf "%--20s%--15s%s\n", "-------", "-----", "-----"
-    check = nil
     @services.each do |service|
-      if service.status_commands.empty? # kill processes the old way
-        if service.pidfiles.empty?
-          pids = service.processes
-        else
-          pids = service.pidfiles.collect do |pidfile|
-            begin
-              File.read(pidfile)
-            rescue
-            end
-          end
-        end
-        if pids.all? do |pid|
-          begin
-            Process::kill 0, pid
-            check = :running 
-          rescue
-            check = :stopped
-          end
-        end
-        end
-      else # use provided status commands
-        service.status_commands.all? do |command|
-          fork { exec command }
-        end
-      end
-      #puts "#{service.name}\t#{service.state.to_s}\t#{result}"
-      result = ""
-      if service.state != :uninitialized
-        if check == service.state
-          result = "OK"
-        else
-          result = "NOK"
-        end
-      end
-      printf "%-20s%-15s%s\n", service.name, service.state.to_s, result
+      state = status_service service
+      printf "%-20s%-15s%s\n", service.name, service.state.to_s, state.to_s
     end
   end
 
@@ -110,10 +143,26 @@ class Controller
       printf "%-20s%s\n", service.name, service.file
     end
   end
+
+  def synchronize_service(service) # ensure service state match persistent state, ie start services in running state
+    @states.transaction(true) do # read-only
+      begin
+        if @states[service.name] == :running
+          state = status_service service
+          if state != :running
+            start_service service
+          end
+        end
+      rescue
+      end
+    end
+  end
 end
 
 
 
+
+# container class for a (single despite the legacy arrays) service
 
 class Service
   attr_reader :description, :name, :start_commands, :stop_commands, :status_commands, :pidfiles
@@ -175,7 +224,8 @@ controller.status
 puts ""
 controller.start
 puts "waiting..."
-sleep 30
+sleep 10
+controller = Controller.new("services")
 controller.status
 puts ""
 controller.stop
